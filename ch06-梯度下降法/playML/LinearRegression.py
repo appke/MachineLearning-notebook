ef570329549079c3b20c62b2ef09818255a64c9c
muliang001 <anglimu@163.com>
2019-01-11 17:30:15 +0800

    自己封装梯度下降法

--- ch06-梯度下降法/playML/LinearRegression.py
+++ ch06-梯度下降法/playML/LinearRegression.py
@@ -1,27 +1,31 @@
 import numpy as np
 from .metrics import r2_score
 
+
 class LinearRegression:
 
     def __init__(self):
         """初始化Linear Regression模型"""
-        self.coef_ = None
-        self.intercept_ = None
-        self._theta = None
+        self.coef_ = None # 系数
+        self.intercept_ = None # 截距
+        self._theta = None # 私有变量
 
     def fit_normal(self, X_train, y_train):
         """根据训练数据集X_train, y_train训练Linear Regression模型"""
         assert X_train.shape[0] == y_train.shape[0], \
             "the size of X_train must be equal to the size of y_train"
 
+        # 横向上多加1列，全是1
         X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
         self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y_train)
-
+        
         self.intercept_ = self._theta[0]
         self.coef_ = self._theta[1:]
 
         return self
-
+    
+    
+    # 梯度下降法，训练模型
     def fit_gd(self, X_train, y_train, eta=0.01, n_iters=1e4):
         """根据训练数据集X_train, y_train, 使用梯度下降法训练Linear Regression模型"""
         assert X_train.shape[0] == y_train.shape[0], \
@@ -66,41 +70,6 @@ class LinearRegression:
 
         return self
 
-    def fit_sgd(self, X_train, y_train, n_iters=5, t0=5, t1=50):
-        """根据训练数据集X_train, y_train, 使用梯度下降法训练Linear Regression模型"""
-        assert X_train.shape[0] == y_train.shape[0], \
-            "the size of X_train must be equal to the size of y_train"
-        assert n_iters >= 1
-
-        def dJ_sgd(theta, X_b_i, y_i):
-            return X_b_i * (X_b_i.dot(theta) - y_i) * 2.
-
-        def sgd(X_b, y, initial_theta, n_iters, t0=5, t1=50):
-
-            def learning_rate(t):
-                return t0 / (t + t1)
-
-            theta = initial_theta
-            m = len(X_b)
-
-            for cur_iter in range(n_iters):
-                indexes = np.random.permutation(m)
-                X_b_new = X_b[indexes]
-                y_new = y[indexes]
-                for i in range(m):
-                    gradient = dJ_sgd(theta, X_b_new[i], y_new[i])
-                    theta = theta - learning_rate(cur_iter * m + i) * gradient
-
-            return theta
-
-        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
-        initial_theta = np.random.randn(X_b.shape[1])
-        self._theta = sgd(X_b, y_train, initial_theta, n_iters, t0, t1)
-
-        self.intercept_ = self._theta[0]
-        self.coef_ = self._theta[1:]
-
-        return self
 
     def predict(self, X_predict):
         """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
